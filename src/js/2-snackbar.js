65620c325a4fbaf3ee637c2d5e871dc62a7c85ff
alina.vinnik <alinavinnikhgfff@gmail.com>
2026-03-11 12:19:44 +0200

    update

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,12 +4,31 @@ import 'izitoast/dist/css/iziToast.min.css';
 const form = document.querySelector('.form');
 const inputDelay = document.querySelector('.docinput-delay');
 const inputFulfilled = document.querySelector('[value="fulfilled"]');
+const inputRejected = document.querySelector('[value="rejected"]');
 
 form.addEventListener('submit', handleSubmit);
 function handleSubmit(e) {
   e.preventDefault();
   const timeDelay = Number(inputDelay.value);
-  const userMessage = createPromise(timeDelay);
+  const fulfilledChecked = inputFulfilled.checked;
+  const rejectedChecked = inputRejected.checked;
+
+  const userMessage = createPromise(timeDelay, fulfilledChecked,import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector('.docinput-delay');
+const inputFulfilled = document.querySelector('[value="fulfilled"]');
+const inputRejected = document.querySelector('[value="rejected"]');
+
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
+  const timeDelay = Number(inputDelay.value);
+  const fulfilledChecked = inputFulfilled.checked;
+  const rejectedChecked = inputRejected.checked;
+
+  const userMessage = createPromise(timeDelay, fulfilledChecked);
   userMessage
     .then(res =>
       iziToast.show({
@@ -27,12 +46,41 @@ function handleSubmit(e) {
     );
 }
 
-function createPromise(delay) {
+function createPromise(delay, fulfilledChecked, rejectedChecked) {
   const promise = new Promise((res, rej) =>
     setTimeout(() => {
-      if (inputFulfilled.checked) {
+      if (fulfilledChecked) {
         res(delay);
-      } else {
+      } else if (rejectedChecked) {
+        rej(delay);
+      }
+    }, delay)
+  );
+  return promise;
+});
+  userMessage
+    .then(res =>
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${res} ms`,
+        position: 'topLeft',
+        backgroundColor: 'rgba(9, 189, 19, 0.8)',
+      })
+    )
+    .catch(rej =>
+      iziToast.show({
+        message: `❌ Rejected promise in ${rej} ms`,
+        position: 'topLeft',
+        backgroundColor: 'rgba(197, 26, 20, 0.8)',
+      })
+    );
+}
+
+function createPromise(delay, fulfilledChecked, rejectedChecked) {
+  const promise = new Promise((res, rej) =>
+    setTimeout(() => {
+      if (fulfilledChecked) {
+        res(delay);
+      } else if (rejectedChecked) {
         rej(delay);
       }
     }, delay)
