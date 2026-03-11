c71fcf074d33193cbfd3449a13f68b685b116418
alina.vinnik <alinavinnikhgfff@gmail.com>
2026-03-11 11:20:01 +0200

    task-2 snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector('.docinput-delay');
+const inputFulfilled = document.querySelector('[value="fulfilled"]');
+const inputRejected = document.querySelector('[value="rejected"]');
+// const btnFormSbm = document.querySelector('.btn-submit');
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
+  CreatePromice();
+}
+
+function CreatePromice() {
+  const timeDelay = Number(inputDelay.value);
+  const promise = new Promise((res, rej) =>
+    setTimeout(() => {
+      if (inputFulfilled.checked) {
+        res(
+          iziToast.show({
+            message: `✅ Fulfilled promise in ${timeDelay} ms`,
+            position: 'topLeft',
+            backgroundColor: 'rgba(9, 189, 19, 0.8)',
+          })
+        );
+      } else {
+        rej(
+          iziToast.show({
+            message: `❌ Rejected promise in ${timeDelay}ms`,
+            position: 'topLeft',
+            backgroundColor: 'rgba(197, 26, 20, 0.8)',
+          })
+        );
+      }
+    }, timeDelay)
+  );
+  return promise;
+}
